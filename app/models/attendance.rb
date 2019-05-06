3d02f1bf4508f7a1821d19e2227a0493c5a9410e
Toww <th.guilmin@gmail.com>
2019-05-06 16:11:37 +0200

    Event message OK

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,11 @@
 class Attendance < ApplicationRecord
+	after_create :attendance_send
+
 	belongs_to :user
 	belongs_to :event
+
+	def attendance_send
+		AttendanceMailer.participation_email(self.user, self.event).deliver_now
+	end
+
 end
